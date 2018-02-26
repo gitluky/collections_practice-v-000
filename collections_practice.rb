c327fe573305c24e457e1d1c50daddb38d8631ab
Jonathan Luk <jluk87@gmail.com>
2018-02-26 21:13:03 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,7 +38,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each {|word| word += "s"}
+  array.each {|word| word[word.length-1] = 's'}
 end
 
 puts add_s(["hand","feet", "knee", "table"])
